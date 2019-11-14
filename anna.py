a585420b7a824aee7ff388ab4796e6b52ff5c31c
Anil-account <Thapaanil62@gmail.com>
2019-11-14 08:37:31 +0545

    small change

--- anna.py
+++ anna.py
@@ -4,5 +4,6 @@ english =['86.0','67.0','87.0']
 for i in range(len(name)):
     if name[i]=='anna':
         a=i
-print("the math mark is", math[a])
+print("the math mark is", math[a])#print result
 print("the english mark is", english[a])
+#Rupendra
